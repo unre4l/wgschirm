e3fcc971299070161c0ea18ba14317ae89ba58d4
Jonathan Wischhusen <jonathan.wischhusen@gmail.com>
2021-02-22 02:22:05 +0100

    fix lint

--- dashboard/.eslintrc.js
+++ dashboard/.eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     "airbnb"
   ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warning" : "off",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "linebreak-style": 0,
     strict: 0,
