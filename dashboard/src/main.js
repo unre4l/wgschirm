170388f1b56cc114b3c69ae286b7d7a68b43950f
Jonathan Wischhusen <jonathan.wischhusen@gmail.com>
2021-02-22 02:23:59 +0100

    fix path

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -1,11 +1,11 @@
-import Vue from 'vue';
-import VueSSE from './vue-sse.js';
-import App from './App.vue';
+import Vue from "vue";
+import VueSSE from "./vue-sse.js";
+import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
-Vue.use(VueSSE, 'http://localhost:9999/events'); // http://localhost:9999/events while developing on localhost
+Vue.use(VueSSE, "/events"); // http://localhost:9999/events while developing on localhost
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
