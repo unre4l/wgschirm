7dbc8d112d589f9623e3d96d83457db0916f51ee
Jonathan Wischhusen <jonathan.wischhusen@gmail.com>
2021-02-23 23:44:26 +0100

    fix linting

--- dashboard/.eslintrc.js
+++ dashboard/.eslintrc.js
@@ -1,46 +1,46 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     // 'plugin:vue/essential',
     // '@vue/standard',
-    "plugin:vue/recommended",
-    "airbnb"
+    'plugin:vue/recommended',
+    'airbnb',
   ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "linebreak-style": 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'linebreak-style': 0,
     strict: 0,
-    "import/extensions": ["off", "never"],
-    "import/no-dynamic-require": 0,
-    "import/no-extraneous-dependencies": 0,
-    "vue/max-attributes-per-line": 0,
-    "no-implicit-coercion": [
-      "off",
+    'import/extensions': ['off', 'never'],
+    'import/no-dynamic-require': 0,
+    'import/no-extraneous-dependencies': 0,
+    'vue/max-attributes-per-line': 0,
+    'no-implicit-coercion': [
+      'off',
       {
         boolean: false,
         number: false,
         string: true,
-        allow: []
-      }
+        allow: [],
+      },
     ],
-    "no-unused-vars": [
+    'no-unused-vars': [
       1,
       {
-        vars: "local",
-        args: "none"
-      }
+        vars: 'local',
+        args: 'none',
+      },
     ],
-    "no-param-reassign": 0,
-    "no-new": 0,
-    "default-case": 0
+    'no-param-reassign': 0,
+    'no-new': 0,
+    'default-case': 0,
   },
   parserOptions: {
-    parser: "babel-eslint",
-    ecmaVersion: "2017",
-    sourceType: "module"
-  }
+    parser: 'babel-eslint',
+    ecmaVersion: '2017',
+    sourceType: 'module',
+  },
 };
